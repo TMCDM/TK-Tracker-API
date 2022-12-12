cb2fba07d7f420ae28f36b5f8a97094bf913e38b
DivSlingerX <taylor@taylororeilly.dev>
2022-12-12 03:37:35 -0500

    add numeric check for jobnumber

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const express = require('express')
 const path = require('path')
+const { isNumeric } = require('./utils/isNumeric')
 
 const { getOrderByJobNumber } = require('./db-operations')
 
@@ -15,35 +16,41 @@ app.use(cors())
 
 // Set up logging
 if (app.get('env') == 'production') {
-    path.join(__dirname, 'access.log'), { flags: 'a' }
+	path.join(__dirname, 'access.log'), { flags: 'a' }
 } else {
-    app.use(morgan('dev')) //log to console on development
+	app.use(morgan('dev')) //log to console on development
 }
 
 app.post('/getOrderByJobNumber', express.json(), async (req, res) => {
-    const jobNumber = req.body.jobNumber
+	const jobNumber = req.body.jobNumber
 
-    if (!jobNumber) {
-        return res.json({ error: 'INVALID_INPUT' })
-    }
+	if (!jobNumber || !isNumeric(jobNumber)) {
+		return res.json({ error: 'INVALID_INPUT' })
+	}
 
-    //get the customer and their boxes
-    const data = await getOrderByJobNumber(jobNumber).catch((err) => {
-        res.json(err)
-    })
+	//get the customer and their boxes
+	const data = await getOrderByJobNumber(jobNumber).catch((err) => {
+		res.json(err)
+	})
 
-    let final = {
-        customer: data.customer,
-        boxes: data.boxes,
-        rawData: data.rawData,
-        error: '',
-    }
 
-    return res.json(final)
+
+
+
+
+
+	let final = {
+		customer: data.customer,
+		boxes: data.boxes,
+		rawData: data.rawData,
+		error: '',
+	}
+
+	return res.json(final)
 })
 
 app.get('/__tmc_test__', (req, res) => {
-    return res.json({ msg: 'OK' })
+	return res.json({ msg: 'OK' })
 })
 
 app.listen(3000)
